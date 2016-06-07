141b1938bb7168aee72256159ba6869b4841ab6f
Simon Prévost <sprevost@mirego.com>
2016-06-07 08:47:27 -0400

    Update credo to 0.4.1

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule JsonapiKit.Mixfile do
       {:plug, "~> 1.1.0"},
       {:ex_doc, "~> 0.11", only: :dev},
       {:earmark, "~> 0.1", only: :dev},
-      {:credo, "~> 0.3", only: [:dev, :test]},
+      {:credo, "~> 0.4", only: [:dev, :test]},
       {:dialyxir, "~> 0.3.3", only: [:dev, :test]}
     ]
   end
