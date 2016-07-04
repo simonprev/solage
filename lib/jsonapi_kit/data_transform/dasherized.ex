0bc0e12a0d49b029b07625446e01a4528bcc0299
Simon Prévost <sprevost@mirego.com>
2016-07-04 19:09:28 -0400

    Support atom and other format for dasherized data transformer

--- lib/jsonapi_kit/data_transform/dasherized.ex
+++ lib/jsonapi_kit/data_transform/dasherized.ex
@@ -5,6 +5,11 @@ defmodule Solage.DataTransform.Dasherized do
 
   @behaviour Solage.DataTransform.Transformer
 
-  def decode_key(key), do: String.replace(key, "-", "_")
-  def encode_key(key), do: String.replace(key, "_", "-")
+  def decode_key(key) when is_atom(key), do: decode_key(to_string(key))
+  def decode_key(key) when is_binary(key), do: String.replace(key, "-", "_")
+  def decode_key(key), do: key
+
+  def encode_key(key) when is_atom(key), do: encode_key(to_string(key))
+  def encode_key(key) when is_binary(key), do: String.replace(key, "_", "-")
+  def encode_key(key), do: key
 end
